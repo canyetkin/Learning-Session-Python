3465cb0335091853ae7162b8cdca279f354a5678
canyetkin <102603350+canyetkin@users.noreply.github.com>
2022-03-29 17:19:16 +0300

    Add files via upload

---
+++ print komutları.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+print("Merhaba \tben can")
+
+
+# In[19]:
+
+
+print('merhaba \nben can')
+
+
+# In[1]:
+
+
+print("Benim adım {}".format('Can'))
+
+
+# In[2]:
+
+
+print("Benim adım {},yasim {}".format('Can','20'))
+
+
+# In[3]:
+
+
+print("Benim adım {0}, yasim {1}".format('Can','20'))
+
+
+# In[4]:
+
+
+print("Benim adım {ad}, yasim {yas}".format(ad='Can', yas=20))
+
+
+# In[8]:
+
+
+x='can'
+y='20'
+print("benim adım {ad},yasim {yas}".format(ad=x, yas=y))
+
