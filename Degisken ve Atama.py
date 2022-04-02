e33ed93d2f3bda2f7bbaac3abeb43a584d88fecd
canyetkin <102603350+canyetkin@users.noreply.github.com>
2022-04-02 15:57:52 +0300

    Add files via upload

---
+++ Degisken ve Atama.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#kabul edilmeyen degisken atama senaryoları:
+
+#degisken sayı ile baslayamaz
+#degisken bosluk iceremez
+#sembol icermemelidir(pyton icin görevi tanımlı degiskenler)
+#rezerve edilmis isimler kullanamayız
+
+
+# In[4]:
+
+
+sayi = 10
+print(sayi)
+
+
+# In[5]:
+
+
+sayi=sayi+1
+print(sayi)
+
+
+# In[6]:
+
+
+print(sayi)
+
+
+# In[11]:
+
+
+sayi_ilk = sayi
+sayi=sayi_ilk + 1
+print(sayi)
+print(sayi_ilk)
+
+
+# In[14]:
+
+
+x=5
+y=6
+print(x-y)
+
---
+++ veri tipleri tekrar.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+strvar = "Merhaba"
+print(strvar)
+
+
+# In[5]:
+
+
+print(strvar[0])
+print(strvar[2])
+
+
+# In[6]:
+
+
+print(strvar[2:])
+
+
+# In[7]:
+
+
+print(strvar[2:5])
+
+
+# In[4]:
+
+
+print(strvar[2])
+
+
+# In[11]:
+
+
+print(strvar[:])
+
+
+# In[12]:
+
+
+print(strvar[2:])
+
+
+# In[15]:
+
+
+print(strvar[1:5:2])
+
+
+# In[16]:
+
+
+len(strvar)
+
+
+# In[12]:
+
+
+strvar = "hello"
+print(strvar + " Mr. Developer")
+strvar = strvar + " Mr. Developer "
+
+
+# In[43]:
+
+
+print(strvar * 4)
+
+
+# In[13]:
+
+
+strvar
+
+
+# In[46]:
+
+
+type(strvar)
+
+
+# In[14]:
+
+
+print(strvar.upper())
+print(strvar.lower())
+print(strvar)
+
+
+# In[9]:
+
+
+strvar = strvar.upper()
+print(strvar)
+
+
+# In[69]:
+
+
+print(strvar.split())
+print(strvar.split('R'))
+
+
+# In[15]:
+
+
+print(strvar.split())
+print(strvar.split(sep="o", maxsplit=2))
+
+
+# In[16]:
+
+
+print(strvar)
+
+
+# In[19]:
+
+
+print(strvar.split())
+strvar = strvar.upper()
+print (strvar)
+print(strvar.split())
+
